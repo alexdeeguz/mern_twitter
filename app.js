cf13490e1f56c4363094eea3d2bf77dd0a82c5fb
alex <alexdg316@gmail.com>
2020-05-03 17:13:10 -0700

    Connect to mongoDB and create users and tweets routes

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI
+const users = require('./routes/api/users')
+const tweets = require('./routes/api/tweets')
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('connected to mongoDB'))
+    .catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
+
+app.use('/api/users', users)
+app.use('/api/tweets', tweets)
 
 const port = process.env.PORT || 5000
 
